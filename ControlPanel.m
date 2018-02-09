56961af8a8fb792ffcabd365570dec4ff77a8137
Mitchell <mcobb12@uncc.edu>
2018-02-09 17:11:45 -0500

    Changed getAvailableComPort for mac

--- ControlPanel.m
+++ ControlPanel.m
@@ -51,7 +51,7 @@ function ControlPanel_OpeningFcn(hObject, eventdata, handles, varargin)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to ControlPanel (see VARARGIN)
-addpath([pwd '\functions']);
+addpath(genpath(pwd));
 % Choose default command line output for ControlPanel
 handles.output = hObject;
 
